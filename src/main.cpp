04bc47842f8a382122094741591a308c1e6cd004
Viktor Sandström <sandstrom.viktor@gmail.com>
2023-04-08 23:24:23 +0200

    cleaned

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,11 @@
 #include "WiFi.h" // <-------------------- beware!!! W i F i . h, remember the capital 'F'
 #include <OSCMessage.h>
 #include "../credentials.c" // <---------- set wifi credentials here!
-
-#define LED 2
-#define CV 23
-// #define Wifi WiFi
-
-
+                            // SSID (string), 
+                            // PASS (string),
+                            // PORT (integer)
+#define LED 2 // <--- Built-In LED
+#define CV 23 // <--- PWD-capable GPIO next to GND-pin
 #ifdef DEBUG
   #define D(x) x
 #else 
@@ -16,11 +15,10 @@
   
 WiFiUDP udp;
 const int update_rate = 8;
-const int channel = 0; // <--- chooses which of the PWM units on the ESP32 to use
-const int freq = 5000; // <--- the frequency of the pulse
-const int resolution = 8; // <--- on ESP32 the useful bits are 16.
+const int channel = 0; // <----- chooses which of the PWM units on the ESP32 to use
+const int freq = 5000; // <----- the frequency of the pulse
+const int resolution = 8; // <-- the resolution is 8 bit : 0 - 255  
 
-// Change to analogWrite message and set to different OSC messages, "fwd" / "bck"
 void ledToggle(OSCMessage &msg) {
   switch (msg.getInt(0)) {
     case 0:
@@ -32,14 +30,11 @@ void ledToggle(OSCMessage &msg) {
   }
 }
 
-// Callback for variable voltage output. NEEDS TESTING!
 void setCV(OSCMessage &msg) {
   // the voltage 0V - 3.3V is represented by 0 - 255 
-  // to get up to the voltage required, perhaps a voltage regulator is required.
+  // to get the voltage required, perhaps a voltage regulator is required.
   float cv = msg.getInt(0);
-  // float inc = 255.0 / 100.0; // converts 0 - 255 values to a scale of 0 - 100% of 3.3V
-  // int value = (int)(inc * cv + 0.5);
-  D(Serial.printf("Dimmer value: %i\n", value);)
+  D(Serial.printf("Dimmer value: %d\n", cv);)
   ledcWrite(channel, cv); 
 }
 
@@ -54,10 +49,9 @@ void receiveMessage() {
     Serial.println("");
     if (!msg.hasError()) {
       msg.dispatch("/cv", setCV);
-      msg.dispatch("/led", ledToggle);
       // finetune messages to just do one speed, reverse etc.
+      msg.dispatch("/led", ledToggle);
     } 
-    //else { auto error = inmsg.getError(); }
   }
 }
 
@@ -75,24 +69,22 @@ void connected() {
 }
 
 void setup() {
-  // put your setup code here, to run once:
-  WiFi.mode(WIFI_STA);
+  // Use serial for debugging purpose
+  Serial.begin(115200);
+  // Output pin setup
   pinMode(LED, OUTPUT);
   ledcSetup(channel, freq, resolution);
   ledcAttachPin(CV, channel);
-  const char* ssid = SSID;
-  const char* password = PASS;
-  unsigned int port = 8888;
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
+  // Wifi client setup
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, PASS);
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print("Retry connection\n");
     delay(500);
   }
-
-  // If WiFi.status = 
   connected();
-  udp.begin(port);
+  udp.begin(PORT);
+  // Setup output pins
 }
 
 void loop() {
